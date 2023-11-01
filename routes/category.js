aa26ac438520604a4cbce0a91586db3122f51d2e
nvdinh185 <nvdinh0766777123@gmail.com>
2023-11-01 18:48:07 +0700

    baitap

--- routes/category.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const categoryController = require('../controllers/CategoryController');
-
-router.get('/', categoryController.getListCats);
-router.get('/catbyid', categoryController.getCatById);
-
-module.exports = router;
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const newsRouter = require('./news');
-const categoryRouter = require('./category');
-
-function route(app) {
-    app.use('/news', newsRouter);
-    app.use('/cat', categoryRouter);
-}
-
-module.exports = route;
