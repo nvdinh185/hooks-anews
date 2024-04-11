2a2445349c8640c014edd8afa4b1a7fd60e1f1f8
nvdinh185 <nvdinh0766777123@gmail.com>
2024-04-11 10:22:32 +0700

    x

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ function Header() {
     return (
         <div id="header">
             <div className="header-left fl">
-                <a href="index.html" title=""><img src="./images/logo_edu.png" alt="" /></a>
+                <a href="index.html" title=""><img src="/images/logo_edu.png" alt="" /></a>
             </div>
             <div className="header-right fr">
-                <a href="index.html" title=""><img src="./images/banner.jpg" alt="" /></a>
+                <a href="index.html" title=""><img src="/images/banner.jpg" alt="" /></a>
             </div>
             <div className="clr"></div>
         </div>
